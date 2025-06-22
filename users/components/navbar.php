39d9ee3a39cca75b444f32c6a6d76441c0988e33
TrendoD <trdipu@gmail.com>
2025-06-22 13:51:09 +0000

    Update navbar link color to use secondary text color; remove redundant hover style

--- users/components/navbar.php
+++ users/components/navbar.php
@@ -337,15 +337,11 @@ $db->close();
 
 .pj-navbar-wrapper .nav-links a {
     text-decoration: none;
-    color: var(--text-color);
+    color: var(--text-color-secondary);
     font-weight: 500;
     transition: var(--transition);
     position: relative;
     padding: 5px 0;
-}
-
-.pj-navbar-wrapper .pj-navbar-header.scrolled .nav-links a {
-    color: var(--text-color-secondary);
 }
 
 .pj-navbar-wrapper .nav-links a:hover {
