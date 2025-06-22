8472dd469856345715259d5327e526252b02ab76
TrendoD <trdipu@gmail.com>
2025-06-22 06:03:14 +0000

    fixed the duplicate dropdown arrow issue:

--- register.php
+++ register.php
@@ -221,10 +221,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             line-height: 1.5;
             color: var(--text-color-secondary);
             background-color: #ffffff;
-            background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 12 12'%3E%3Cpath fill='%23333' d='M10.293 3.293L6 7.586 1.707 3.293A1 1 0 00.293 4.707l5 5a1 1 0 001.414 0l5-5a1 1 0 10-1.414-1.414z'/%3E%3C/svg%3E");
-            background-repeat: no-repeat;
-            background-position: right 1rem center;
-            background-size: 12px;
             appearance: none;
             -webkit-appearance: none;
             -moz-appearance: none;
@@ -282,8 +278,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
         
         .form-icon select {
-            padding-right: 3rem;
-            background-position: right 2.5rem center;
+            padding-right: 2.5rem;
         }
         
         .btn {
